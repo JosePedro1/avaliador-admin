e3ebbf6ecbfbaeb7308aa88ba70f9813e76f321f
Jose Pedro <jeremitafilho@gmail.com>
2025-02-28 18:34:51 -0300

    exibir notas final

--- public/script.js
+++ public/script.js
@@ -221,7 +221,7 @@ function exportarTodosPDFs(botao) {
         setTimeout(() => {
             pdf.save(`Todas_as_Tabelas.pdf`);
             alert("Todas as tabelas foram exportadas para um único PDF!");
-           
+            botao.textContent = "Exportar TODOS PDFs Outra Vez?";
             botao.disabled = false;
         }, 2000);
     } else {
